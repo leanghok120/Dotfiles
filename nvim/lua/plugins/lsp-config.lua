f5c74cdff012edce2d135a8871980563fc04690b
leanghok120 <leanghokoeng5@gmail.com>
2024-05-05 12:47:13 +0700

    Update nvim

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -7,7 +7,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver" },
+				ensure_installed = { "lua_ls", "tsserver", "html", "cssls" },
 			})
 		end,
 	},
@@ -17,6 +17,8 @@ return {
 			local lspconfig = require("lspconfig")
 			lspconfig.lua_ls.setup({})
 			lspconfig.tsserver.setup({})
+      lspconfig.html.setup({})
+      lspconfig.cssls.setup({})
 		end,
 	},
 }
