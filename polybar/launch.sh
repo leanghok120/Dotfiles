a9c8e4a170a54e9c27eff295d97228c8bb7b1652
leanghok120 <leanghokoeng5@gmail.com>
2024-05-10 18:45:53 +0700

    Update polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,7 +6,6 @@ killall polybar
 for mon in $(polybar --list-monitors | cut -d":" -f1); do
 		(MONITOR=$mon polybar -q pam1 -c ~/.config/polybar/config.ini) &
 		(MONITOR=$mon polybar -q pam2 -c ~/.config/polybar/config.ini) &
-		(MONITOR=$mon polybar -q pam3 -c ~/.config/polybar/config.ini) &
 		(MONITOR=$mon polybar -q pam4 -c ~/.config/polybar/config.ini) &
 		(MONITOR=$mon polybar -q pam5 -c ~/.config/polybar/config.ini) &
 	done
