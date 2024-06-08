2ee966645ede9220d43dea679a5a0581c0f74762
leanghok120 <leanghokoeng5@gmail.com>
2024-06-08 20:05:18 +0700

    Update neovim (undone)

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,6 +3,8 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+vim.g.mapleader = " "
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -16,4 +18,29 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({}, {})
+require("lazy").setup("plugins")
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
+vim.keymap.set("n", "<leader>sg", builtin.live_grep, {})
+
+-- Keymaps
+vim.keymap.set("n", "<C-n>", "<cmd>Ex<CR>")
+
+-- Catppuccin
+require("catppuccin").setup()
+vim.cmd.colorscheme("catppuccin")
+
+-- Treesitter
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = { "lua", "bash", "vim" },
+  auto_install = true,
+  hightlight = {
+    enable = true
+  },
+
+  indent = {
+    enable = true 
+  },
+})
---
+++ nvim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = '0.1.6',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate"
+  }
+}
