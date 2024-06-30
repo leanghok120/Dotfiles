8b1a232499ac06fbbd950747bd93414243758031
leanghok120 <leanghokoeng5@gmail.com>
2024-06-30 16:55:51 +0700

    Add keybindings

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -4,13 +4,15 @@ return {
     config = function()
       require("gitsigns").setup()
 
-      vim.keymap.set("n", "<leader>gp", "<cmd>Gitsigns preview_hunk<CR>")
+      vim.keymap.set("n", "<leader>gP", "<cmd>Gitsigns preview_hunk<CR>")
     end,
   },
   {
     "tpope/vim-fugitive",
     config = function()
       vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+      vim.keymap.set("n", "<leader>gp", "<cmd>Git push<CR>")
+      vim.keymap.set("n", "<leader>gc", "<cmd>Git commit<CR>")
     end,
   },
 }
