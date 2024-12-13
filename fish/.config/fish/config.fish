9397dfb284c8941fe10f548cc7c3996af4179746
Leanghok <leanghokoeng5@gmail.com>
2024-12-13 15:50:47 +0700

    Some stuff after reinstalling

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,3 @@
 if status is-interactive
-  set -Ux fish_greeting
-
-  export GOROOT="/usr/local/go/"
-  export GOPATH="$HOME/go"
-  export PATH="$HOME/.tmuxifier/bin:$PATH"
-  export PATH="$HOME/.cargo/bin:$PATH"
-  export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
-  export PATH="$HOME/.local/scripts:$PATH"
-  export EDITOR='nvim'
-  export MANPAGER="nvim +Man!"
+    # Commands to run in interactive sessions can go here
 end
