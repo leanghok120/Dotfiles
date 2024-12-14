e72703803dd28f4ba915685f2efc48d4d29f45e7
Leanghok <leanghokoeng5@gmail.com>
2024-12-14 18:04:01 +0700

    Reconfigure fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,12 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+  set -Ux fish_greeting
+
+  export GOROOT="/usr/local/go/"
+  export GOPATH="$HOME/go"
+  export PATH="$HOME/.tmuxifier/bin:$PATH"
+  export PATH="$HOME/.cargo/bin:$PATH"
+  export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"
+  export PATH="$HOME/.local/scripts:$PATH"
+  export EDITOR='nvim'
+  export MANPAGER="nvim +Man!"
 end
